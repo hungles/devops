e3e0ccf1d444bccd46526057a3b908a9e017713f
Sebastian Carmona <scarmona04@hotmail.com>
2025-01-22 22:29:26 -0500

    Mejorando los nombres en los objetos

---
+++ k8/proyects/kafka-kraft/deployments/create.sh
@@ -0,0 +1,3 @@
+kubectl apply -f api-json/json-rest.yml
+kubectl apply -f kafka/kafka.yml
+kubectl apply -f kafka/kafka-connect.yml
---
+++ k8/proyects/kafka-kraft/deployments/delete.sh
@@ -0,0 +1,3 @@
+kubectl delete -f api-json/json-rest.yml
+kubectl delete -f kafka/kafka.yml
+kubectl delete -f kafka/kafka-connect.yml
